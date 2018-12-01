1069fe77cb8dd97f993e8e99c6f4246b508ca0fd
nim94 <nikomoro1994@gmail.com>
2018-12-01 12:15:53 +0100

    header, footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,20 @@
 <footer class="content-info">
   <div class="container">
-    @php dynamic_sidebar('sidebar-footer') @endphp
+    <div class="row">
+      <div class="col-sm-12 col-lg-4 text-footer">
+        <h4>Policardia Teatro</h4>
+        <a href="mailto:promozione.policardiateatro@gmail.com">promozione.policardiateatro@gmail.com</a>
+        <a href="mailto:coord.policardiateatro@gmail.com">coord.policardiateatro@gmail.com</a>
+      </div>
+      <div class="col-sm-12 col-lg-4 social_footer">
+        <div><img src="{{get_stylesheet_directory_uri()}}/assets/images/bianco_facebook.png" alt="link social facebook"></div>
+        <div><img src="{{get_stylesheet_directory_uri()}}/assets/images/bianco_instagram.png" alt="link social instagram"></div>
+        <div><img src="{{get_stylesheet_directory_uri()}}/assets/images/bianco_Youtube.png" alt="link social youtube"></div> 
+      </div>
+      <div class="col-sm-12 col-lg-4 funder35">
+        <div><img src="{{get_stylesheet_directory_uri()}}/assets/images/marchi_funder35.png" alt="Marchi Funder35"></div>
+        <div>La Policardia Teatro è <br />sostenuta de Funder35</div>
+      </div>
+    </div>
   </div>
 </footer>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,38 @@
 <header class="banner">
   <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="nav-primary">
+    <div class="social_links">
+      <a href="#" class="social_link"><img src="{{get_stylesheet_directory_uri()}}/assets/images/tasto_facebook.png" alt="facebook page link" class="social_img"></a>
+      <a href="#" class="social_link"><img src="{{get_stylesheet_directory_uri()}}/assets/images/tasto_instagram.png" alt="instagram page link" class="social_img"></a>
+      <a href="#" class="social_link"><img src="{{get_stylesheet_directory_uri()}}/assets/images/tasto_youtube.png" alt="youtube page link" class="social_img"></a>
+    </div>
+    <div class="center">
+      <a class="brand" href="{{ home_url('/') }}"><img src="{{get_stylesheet_directory_uri()}}/assets/images/logo_teatro_policardia.png" alt="logo teatro Policardia" class="logo"></a>
+    </div>
+    {{-- Navbar desktop --}}
+    <nav class="navbar-desktop">
       @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
       @endif
     </nav>
+    <!--Navbar phone-->
+    <nav class="navbar navbar-light purple lighten-4 mb-4">
+  
+      <!-- Collapse button -->
+      <button class="navbar-toggler toggler-example purple darken-3" type="button" data-toggle="collapse"
+        data-target="#navbarSupportedContent41" aria-controls="navbarSupportedContent41" aria-expanded="false"
+        aria-label="Toggle navigation"><span class="navbar-toggler-icon black-text"><i class="fa fa-bars fa-1x"></i></span></button>
+    
+      <!-- Collapsible content -->
+      <div class="collapse navbar-collapse" id="navbarSupportedContent41">
+    
+        @if (has_nav_menu('primary_navigation'))
+          {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+        @endif
+    
+      </div>
+      <!-- Collapsible content -->
+    
+    </nav>
+  <!--/.Navbar-->
   </div>
 </header>
