6abb7397e80dab4bc2847974796f0d22c2821f0d
nim94 <nikomoro1994@gmail.com>
2018-12-03 16:46:25 +0100

    homepage fixed

--- resources/views/page-home.blade.php
+++ resources/views/page-home.blade.php
@@ -5,7 +5,7 @@
         <div class="row la_policardia">
             @php $post_policardia = get_post(7); @endphp
             <div class="col-sm-12 col-md-6 section_title_container left"> 
-                <a href="{!! get_permalink($post_policardia) !!}"><h2 class="section_title left">{{ $post_policardia->post_title }}</h2></a>
+                <a href="{!! get_permalink($post_policardia) !!}" class="section_title_link left"><h2 class="section_title left">{{ $post_policardia->post_title }}</h2></a>
                 <p class="section_toggle_mobile left">{{ $post_policardia->post_excerpt }}</p>    
             </div>    
             <div class="col-sm-12 col-md-6 slider">
@@ -31,7 +31,7 @@
         <div class="row la_compagnia">
             @php $post_policardia = get_post(9); @endphp
             <div class="col-sm-12 col-md-6 section_toggle_desktop">
-                <a href="{!! get_permalink($post_policardia) !!}"><h2 class="section_title right">{{ $post_policardia->post_title }}</h2></a>
+                <a href="{!! get_permalink($post_policardia) !!}" class="section_title_link right"><h2 class="section_title right">{{ $post_policardia->post_title }}</h2></a>
             </div>
             <div class="col-sm-12 col-md-6 slider">
                 <div class="owl-carousel owl-theme owl-carousel-home">
@@ -50,14 +50,14 @@
                 </div>
             </div>
             <div class="col-sm-12 col-md-6 section_title_container right"> 
-                <a href="{!! get_permalink($post_policardia) !!}" class="section_toggle_mobile"><h2 class="section_title right">{{ $post_policardia->post_title }}</h2></a>
+                <a href="{!! get_permalink($post_policardia) !!}" class="section_toggle_mobile section_title_link right"><h2 class="section_title right">{{ $post_policardia->post_title }}</h2></a>
                 <p class="section_excerpt right">{{ $post_policardia->post_excerpt }}</p>    
             </div>    
         </div>
         <div class="row cci">
             @php $post_policardia = get_post(11); @endphp
             <div class="col-sm-12 col-md-6 section_title_container left"> 
-                <a href="{!! get_permalink($post_policardia) !!}"><h2 class="section_title left">{{ $post_policardia->post_title }}</h2></a>
+                <a href="{!! get_permalink($post_policardia) !!}" class="section_title_link left"><h2 class="section_title left">{{ $post_policardia->post_title }}</h2></a>
                 <p class="section_toggle_mobile left">{{ $post_policardia->post_excerpt }}</p>    
             </div>    
             <div class="col-sm-12 col-md-6 slider">
@@ -83,7 +83,7 @@
         <div class="row formazione">
             @php $post_policardia = get_post(13); @endphp
             <div class="col-sm-12 col-md-6 section_toggle_desktop">
-                <a href="{!! get_permalink($post_policardia) !!}" class="section_toggle_mobile"><h2 class="section_title right">{{ $post_policardia->post_title }}</h2></a>
+                <a href="{!! get_permalink($post_policardia) !!}" class="section_title_link right"><h2 class="section_title right">{{ $post_policardia->post_title }}</h2></a>
             </div>
             <div class="col-sm-12 col-md-6 slider">
                 <div class="owl-carousel owl-theme owl-carousel-home">
@@ -102,7 +102,7 @@
                 </div>
             </div>
             <div class="col-sm-12 col-md-6 section_title_container right"> 
-                <a href="{!! get_permalink($post_policardia) !!}" class="section_toggle_mobile"><h2 class="section_title right">{{ $post_policardia->post_title }}</h2></a>
+                <a href="{!! get_permalink($post_policardia) !!}" class="section_toggle_mobile section_title_link right"><h2 class="section_title right">{{ $post_policardia->post_title }}</h2></a>
                 <p class="section_excerpt right">{{ $post_policardia->post_excerpt }}</p>    
             </div>
         </div>
